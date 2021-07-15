a482f0644f587faf6bd029d295605d32aeeef591
langerb <langerb@users.noreply.github.com>
2021-07-15 14:29:48 +0000

    beautifying and comments

--- test.py
+++ test.py
@@ -4,50 +4,49 @@ from bs4 import BeautifulSoup
 
 
 def countWordinWeb(url, word, depth):
-    
+
+  print("depth:", depth, "url", url)
+
   try:  
     page = requests.get(url)
   except requests.RequestException as e:
-    print("OOPS!! General Error")
     print(str(e))
     return 0
     
   else:
-  
-    if (page.status_code > 200):
-      return 0
-
     soup = BeautifulSoup(page.content, 'html.parser')
 
+    # extract text paragraphs
     pageText = list(soup.find_all('p'))
     raw = []
-
-    count = 1
+    count = 0
+    # make one big string
     for x in pageText:
 	    raw.append(x.get_text())
     fullText = ' '.join(raw)
-    #print(fullText)
+    # count the word
     count = fullText.count(word)
-    #print("Number of occurences: ", count)
 
+    # follow links on page
     if depth <= 1:
       linkObjs = soup.find_all('a', href=True)
       linkSet = set()
       for linkObj in linkObjs:
         linkSet.add(str(linkObj['href']))
 
+      # see if it is a relative link or an absolute link
       for link in linkSet:
         if (link.count("http") == 0):
           sublink = url + link
         else:
           sublink = link
-        newdepth = depth + 1
-        print("depth: ", newdepth, "url: ", sublink)
-        count = count + countWordinWeb(sublink, word, newdepth)
+
+        # recursive call
+        count = count + countWordinWeb(sublink, word, depth + 1)
   
     return count
   
-print("final result: ", countWordinWeb("https://www.microsoft.com", "data", 1))
+print("final result: ", countWordinWeb("https://www.d-one.ai", "data", 1))
 
 #Value
 #Zuehlke: 82
