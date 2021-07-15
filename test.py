b5d9978ca6f4c648140997431b054fa38ade1f7f
langerb <langerb@users.noreply.github.com>
2021-07-15 09:28:18 +0000

    Web scrape test

--- test.py
+++ test.py
@@ -1,1 +1,12 @@
-print("This line will be printed.")
+import requests
+
+from bs4 import BeautifulSoup
+
+page = requests.get("https://zuehlke.com")
+print("GET result: " + str(page.status_code))
+
+soup = BeautifulSoup(page.content, 'html.parser')
+
+text = list(soup.findall('p'))
+for x in text:
+	print(x)
